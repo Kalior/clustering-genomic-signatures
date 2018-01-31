94d385a37b27bfc5c439407f56bd3be67f05781f
Kalior <kalioragus@gmail.com>
2018-01-31 08:38:33 +0100

    Rename char to char_ to avoid confusion over reserved word char

--- src/distance/naive_parameter_sampling.pyx
+++ src/distance/naive_parameter_sampling.pyx
@@ -21,14 +21,14 @@ cdef class NaiveParameterSampling(object):
 
   cdef double _assymmetric_distance(self, left_tree, right_tree, alphabet):
     cdef double s
-    s = sum(self._min_value_of_same_order(right_tree, k, left_tree[k][char], char)
-            for k in left_tree.keys() for char in alphabet)
+    s = sum(self._min_value_of_same_order(right_tree, k, left_tree[k][char_], char_)
+            for k in left_tree.keys() for char_ in alphabet)
 
     s = s / (len(alphabet) * len(left_tree.keys()))
     s = math.sqrt(s)
     return s
 
-  cdef double _min_value_of_same_order(self, tree, context, prob, char):
-    cdef list probabilities = [tree[k][char] for k in tree.keys() if len(k) == len(context)]
+  cdef double _min_value_of_same_order(self, tree, context, prob, char_):
+    cdef list probabilities = [tree[k][char_] for k in tree.keys() if len(k) == len(context)]
     cdef double val = min(math.pow((val - prob), 2) for val in probabilities)
     return val
