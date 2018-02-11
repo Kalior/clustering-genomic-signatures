7d3e68572141a95c58089517aad944167b002335
Erik Norlander <e.olof.norlander@gmail.com>
2018-02-11 22:48:04 +0100

    Some clean up, better names for functions etc
    Added a few TODOs

--- src/distance/statisticalmetric.pyx
+++ src/distance/statisticalmetric.pyx
@@ -27,80 +27,83 @@ cdef class StatisticalMetric(object):
   7. Else exit with no equivalence.
   """
   cpdef double distance(self, left_vlmc, right_vlmc):
-    for p_value in np.arange(0, 0.05, 0.001):
-      left_vlmc.tree = self.remove_larger_probabilities(left_vlmc.tree, p_value)
-      right_vlmc.tree = self.remove_larger_probabilities(right_vlmc.tree, p_value)
+    p_values = np.arange(0, 1, 0.04) # should come from a function
+    for threshhold in p_values:
+      left_vlmc.tree = self.remove_unlikely_transitions(left_vlmc.tree, threshhold)
+      right_vlmc.tree = self.remove_unlikely_transitions(right_vlmc.tree, threshhold)
       if (not self.is_null_model(left_vlmc) and not self.is_null_model(right_vlmc)):
+        # as long as none of the models were null-models, perform an equivalence test
         if self.equivalence_test(left_vlmc, right_vlmc):
-          print("Found equality at p_value " + str(p_value))
-          return p_value
+          print("Found equality at p_value " + str(threshhold))
+          return threshhold
     return 1
 
   cdef bint is_null_model(self, vlmc):
-    # TODO, this is clearly wrong
-    return False
+    # TODO, change this to check if there are any transitions left between any of the
+    # context states.  This will work better if the vlmc is seen as an automata.
     for lookup_prob in vlmc.tree.values():
       if any(prob > 0 for prob in lookup_prob.values()):
         return False
     return True
 
-  cdef dict remove_larger_probabilities(self, tree, p_value):
-    contexts_to_remove = []
+  cdef dict remove_unlikely_transitions(self, tree, threshhold_probability):
+    # TODO, change this to use event probabilites instead of the actual transition probabilites
+    # we should think of the vlmc as an automata where each transition in the automata is a single
+    # event.
+    contexts_without_outgoing_transitions = []
     for context in tree.keys():
-      nbr_greater_probabilities = 0
-      sum_of_other_probabilites = 0
-      for letter in tree[context]:
-        if tree[context][letter] <= p_value:
-          tree[context][letter] = 0
+      nbr_transitions_to_keep = 0
+      sum_probabilites_of_transitions_to_keep = 0
+      for character in tree[context]:
+        if tree[context][character] <= threshhold_probability:
+          # if a probability is smaller than threshhold, set it to zero
+          tree[context][character] = 0
         else:
-          sum_of_other_probabilites += tree[context][letter]
-          nbr_greater_probabilities += 1
-      if nbr_greater_probabilities == 0:
-        contexts_to_remove.append(context)
-      for letter in tree[context]:
-        if tree[context][letter] > p_value:
-          tree[context][letter] = tree[context][letter] / sum_of_other_probabilites
-    for context in contexts_to_remove:
+          # else keep it
+          sum_probabilites_of_transitions_to_keep += tree[context][character]
+          nbr_transitions_to_keep += 1
+      if nbr_transitions_to_keep == 0:
+        # if no transitions left, delete context
+        contexts_without_outgoing_transitions.append(context)
+
+      for character in tree[context]:
+        # re-normalize the transitions probabilites that were kept
+        if tree[context][character] > threshhold_probability:
+          tree[context][character] = tree[context][character] / sum_probabilites_of_transitions_to_keep
+
+    for context in contexts_without_outgoing_transitions:
       tree.pop(context)
     return tree
 
   cdef bint equivalence_test(self, left_vlmc, right_vlmc):
     pre_sample_length = 500
-    cdef str sequence = left_vlmc.generate_sequence(self.sequence_length,
-                                                    pre_sample_length)
+    cdef str sequence = left_vlmc.generate_sequence(self.sequence_length, pre_sample_length)
     # For every starting state,
     possible_contexts = right_vlmc.tree.keys()
     for start_context in possible_contexts:
-      if self.check_from_context(start_context, sequence, right_vlmc):
+      if self.equality_test_given_starting_context(start_context, sequence, right_vlmc):
         return True
     return False
 
-  cdef bint check_from_context(self, start_context, sequence, right_vlmc):
+  cdef bint equality_test_given_starting_context(self, start_context, sequence, right_vlmc):
     current_context = start_context
     # Map (Context -> Int)
-    # Map (Context -> Map (Letter -> Int))
-    context_counters, context_probabilities = self.initialize_counters(right_vlmc,
-                                                                       current_context)
-    for char_ in sequence:
+    # Map (Context -> Map (Character -> Int))
+    context_counters, transition_counters = self.initialize_counters(right_vlmc,                                                                        current_context)
+    for character in sequence:
       context_counters[current_context] += 1
-      context_probabilities[current_context][char_] += 1
-      current_context = self.get_next_context(current_context, char_, right_vlmc)
-      if current_context == "":
+      transition_counters[current_context][character] += 1
+      current_context = self.get_relvant_context(current_context, character, right_vlmc)
+      if current_context == None:
+        # this means that the vlmc could not have produced the next character given its context
+        # exit with no equivalence for this starting context
         return False
 
-    new_vlmc_tree = self.create_pst_by_estimating_probabilities(context_counters, context_probabilities)
+    new_vlmc_tree = self.create_pst_by_estimating_probabilities(context_counters, transition_counters)
     expected_values, observed_values = self.get_expected_observed_values(new_vlmc_tree, right_vlmc, context_counters)
     chisq, p_value = stats.chisquare(f_obs=observed_values, f_exp=expected_values)
     if 1 - p_value < self.significance_level:
       return True
-
-  cdef int nbr_of_non_zero_probabilities(self, vlmc):
-    counter = 0
-    for context, probabilites in vlmc.tree:
-      for char_, prob in probabilites:
-        if prob > 0:
-          counter += 1
-    return counter
 
   cdef tuple get_expected_observed_values(self, estimated_vlmc_tree, original_vlmc, context_count):
     expected_values = []
@@ -109,14 +112,16 @@ cdef class StatisticalMetric(object):
       times_visited_node = context_count[context]
       if times_visited_node > 0:
         alphabet = ["A", "C", "G", "T"]
-        probabilites_original = list(zip(alphabet, list(map(lambda x: original_vlmc.tree[context][x], alphabet ))))
+        transition_probabilitites = list(map(lambda x: original_vlmc.tree[context][x], alphabet ))
+        probabilites_original = list(zip(alphabet, transition_probabilitites))
+
         # find probabilites that are greater than zero
         probs_without_zeros = [item for item in probabilites_original if item[1] > 0]
         # loop through all of these exept one (last)
         for char_prob in probs_without_zeros[:-1]:
-          letter = char_prob[0]
+          character = char_prob[0]
           probability_original_vlmc = char_prob[1]
-          probability_estimation = estimated_vlmc_tree[context][letter]
+          probability_estimation = estimated_vlmc_tree[context][character]
 
           expected_frequency = times_visited_node*probability_original_vlmc
           observed_frequency = times_visited_node*probability_estimation
@@ -124,39 +129,45 @@ cdef class StatisticalMetric(object):
           observed_values.append(observed_frequency)
     return expected_values, observed_values
 
-  cdef dict create_pst_by_estimating_probabilities(self, context_counters, context_probabilites):
+  cdef dict create_pst_by_estimating_probabilities(self, context_counters, transition_probabilities):
     tree = {}
     for context, count in context_counters.items():
       tree[context] = {}
-      for char_ in ["A", "C", "G", "T"]:
+      for character in ["A", "C", "G", "T"]:
         if count > 0:
-          tree[context][char_] = context_probabilites[context][char_] / count
+          tree[context][character] = transition_probabilities[context][character] / count
         else:
-          tree[context][char_] = 0
+          tree[context][character] = 0
 
     return tree
 
-  cdef str get_next_context(self, context_before, next_char, vlmc_to_approximate):
+  cdef str get_relvant_context(self, context_before, next_char, vlmc_to_approximate):
     order = vlmc_to_approximate.order
     possible_contexts = vlmc_to_approximate.tree.keys()
-    tmp_context = next_char + context_before
+    tmp_context = context_before + next_char
     # truncate to _order_ nbr of characters
-    new_context = tmp_context[:order]
+    new_context = tmp_context[-order:]
     # Loop through to find the longest possible context applicable
-    for i in range(order):
-      maybe_context = new_context[:order-i]
+    # TODO, extract this into to vlmc model
+    for i in range(order+1):
+      if not i == order:
+        maybe_context = new_context[-(order-i):]
+      else:
+        maybe_context = ""
       if maybe_context in possible_contexts:
         return maybe_context
-    return ""
+    # returns None if the approximated vlmc does not have any context
+    # that fits the sequence
+    return None
 
   cdef tuple initialize_counters(self, right_vlmc, start_context):
     context_counters = {}  # Map (Context -> Int)
-    context_probabilities = {}  # Map (Context -> Map (Letter -> Int))
+    transition_counters = {}  # Map (Context -> Map (Character -> Int))
     current_context = start_context
     # Initialize counters
     for context in right_vlmc.tree.keys():
       context_counters[context] = 0
-      context_probabilities[context] = {}
-      for char_ in ["A", "C", "G", "T"]:
-        context_probabilities[context][char_] = 0
-    return context_counters, context_probabilities
+      transition_counters[context] = {}
+      for character in ["A", "C", "G", "T"]:
+        transition_counters[context][character] = 0
+    return context_counters, transition_counters
