488453790cfac28c4247a7922a78f990a39cff47
Kalior <kalioragus@gmail.com>
2018-02-21 17:07:48 +0100

    Add directory option to distance function test

--- src/test_distance_function.py
+++ src/test_distance_function.py
@@ -7,28 +7,28 @@ import time
 from get_signature_metadata import get_metadata_for
 
 
-def test_negloglike(sequence_length):
+def test_negloglike(tree_dir, sequence_length):
   d = NegativeLogLikelihood(sequence_length)
-  test_distance_function(d)
+  test_distance_function(d, tree_dir)
 
 
-def test_parameter_sampling():
+def test_parameter_sampling(tree_dir):
   d = NaiveParameterSampling()
-  test_distance_function(d)
+  test_distance_function(d, tree_dir)
 
 
-def test_acgt_content():
+def test_acgt_content(tree_dir):
   d = ACGTContent()
-  test_distance_function(d)
+  test_distance_function(d, tree_dir)
 
 
-def test_stationary_distribution():
+def test_stationary_distribution(tree_dir):
   d = StationaryDistribution()
-  test_distance_function(d)
+  test_distance_function(d, tree_dir)
 
 
-def test_distance_function(d):
-  tree_dir = "../trees"
+
+def test_distance_function(d, tree_dir):
   parse_trees_to_json.parse_trees(tree_dir)
   vlmcs = VLMC.from_json_dir(tree_dir)
   metadata = get_metadata_for([vlmc.name for vlmc in vlmcs])
@@ -88,20 +88,25 @@ if __name__ == '__main__':
   parser.add_argument('--seqlen', type=int, default=1000,
                       help='The length of the sequences that are generated to calculate the likelihood.')
 
+
+  parser.add_argument('--directory', type=str, default='../trees',
+                      help='The directory which contains the trees to be used.')
+
   args = parser.parse_args()
 
   if (args.negative_log_likelihood):
     print('Testing negative log likelihood with a generated sequence of length {}'.format(args.seqlen))
-    test_negloglike(args.seqlen)
+    test_negloglike(args.directory, args.seqlen)
 
   if (args.parameter_sampling):
     print('Testing the measure of estimation error distance function, the parameter based sampling.')
-    test_parameter_sampling()
+    test_parameter_sampling(args.directory)
 
   if (args.acgt_content):
     print("Testing distance based only on acgt content.")
-    test_acgt_content()
+    test_acgt_content(args.directory)
 
   if (args.stationary_distribution):
     print("Testing distance based on the stationary distribution")
-    test_stationary_distribution()
+    test_stationary_distribution(args.directory)
+
