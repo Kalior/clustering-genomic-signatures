beab5dc4dc08e62bdcc60ab5e90c28a9f7f7c431
Kalior <kalioragus@gmail.com>
2018-02-26 14:32:40 +0100

    Add actual frobenius norm function

---
+++ src/distance/frobenius.pyx
@@ -0,0 +1,54 @@
+import numpy as np
+cimport numpy as np
+FLOATTYPE = np.float32
+ctypedef np.float32_t FLOATTYPE_t
+
+cdef class FrobeniusNorm(object):
+  """
+    Distance simply based on the amount of a, c, g, t content in the strings.
+    (Should be stored as transitions from the root node).
+  """
+
+  cdef list alphabet
+
+  def __init__(self, alphabet=['A', 'C', 'G', 'T']):
+    self.alphabet = alphabet
+
+
+  cpdef double distance(self, left_vlmc, right_vlmc):
+    distance = self._frobenius_norm(left_vlmc, right_vlmc)
+    return distance
+
+  cdef double _frobenius_norm(self, original_vlmc, estimated_vlmc):
+    cdef double norm = 0.0
+
+    cdef np.ndarray[FLOATTYPE_t, ndim=2] original_matrix = self._create_matrix(original_vlmc)
+    cdef np.ndarray[FLOATTYPE_t, ndim=2] estimated_matrix = self._create_matrix(estimated_vlmc)
+
+    cdef np.ndarray[FLOATTYPE_t, ndim=2] frobenius_matrix = original_matrix - estimated_matrix
+
+    return np.linalg.norm(frobenius_matrix, ord='fro')
+
+  cdef np.ndarray[FLOATTYPE_t, ndim=2] _create_matrix(self, vlmc):
+    leaf_contexts = self._get_leaf_contexts(vlmc)
+    cdef int rows = len(leaf_contexts)
+    cdef int columns = len(self.alphabet)
+    cdef np.ndarray[FLOATTYPE_t, ndim=2] matrix = np.empty((rows, columns), dtype=FLOATTYPE)
+
+    cdef FLOATTYPE_t val
+
+    for i, context in enumerate(leaf_contexts):
+      for j, character in enumerate(self.alphabet):
+        val = vlmc.tree[context][character]
+        matrix[i, j] = val
+
+    return matrix
+
+  cdef list _get_leaf_contexts(self, vlmc):
+    return list(filter(lambda c: self._is_leaf_context(c, vlmc), list(vlmc.tree.keys())))
+
+  cdef bint _is_leaf_context(self, context, vlmc):
+    possible_leaves = list(map(lambda c: context + c, self.alphabet))
+    # leaf contexts are defined as having no children at all
+    return all(map(lambda leaf: not leaf in vlmc.tree, possible_leaves))
+
