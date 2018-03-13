b7af483746d7a3ef696a470819fc94dfa31c32b3
Kalior <kalioragus@gmail.com>
2018-03-13 17:34:48 +0100

    Add extra distance representation which allows indexing for vlmcs

--- src/clustering/graph_based_clustering.pxd
+++ src/clustering/graph_based_clustering.pxd
@@ -8,12 +8,17 @@ cdef class GraphBasedClustering:
   """
 
   cdef list vlmcs
-  cdef d
+  cdef object d
   cdef str file_name
+  cdef np.ndarray indexed_distances
 
   cpdef tuple cluster(self, clusters)
 
   cdef _cluster(self, G, num_clusters, distances)
+
+  cdef void _make_fully_connected_components(self, G)
+
+  cdef FLOATTYPE_t _find_distance_from_vlmc(self, v1, v2)
 
   cdef np.ndarray[FLOATTYPE_t, ndim=2] _calculate_distances(self)
 
--- src/clustering/graph_based_clustering.pyx
+++ src/clustering/graph_based_clustering.pyx
@@ -14,6 +14,7 @@ cdef class GraphBasedClustering:
     self.vlmcs = vlmcs
     self.d = d
     self.file_name = 'cluster_distances'
+    self.indexed_distances = np.ndarray([len(vlmcs), len(vlmcs)], dtype=FLOATTYPE)
 
   cpdef tuple cluster(self, clusters):
     G = nx.Graph()
@@ -29,12 +30,28 @@ cdef class GraphBasedClustering:
     print("Distance time: {} s".format(distance_time))
 
     self._cluster(G, clusters, distances)
+
+    self._make_fully_connected_components(G)
+
     distance_mean = np.mean(distances, axis=None)
 
     return G, distance_mean
 
-  cdef _cluster(self, G, num_clusters, distances):
-    return
+  cdef void _make_fully_connected_components(self, G):
+    connected_components = nx.connected_components(G)
+    for component in connected_components:
+      for v1 in component:
+        for v2 in component:
+          G.add_edge(v1, v2, weight=self._find_distance_from_vlmc(v1, v2))
+
+  cdef FLOATTYPE_t _find_distance_from_vlmc(self, v1, v2):
+    v1_idx = self.vlmcs.index(v1)
+    v2_idx = self.vlmcs.index(v2)
+    return self.indexed_distances[v1_idx, v2_idx]
+
+  cdef void _cluster(self, G, num_clusters, distances):
+    for (left, right, dist) in distances:
+      G.add_edge(self.vlmcs[int(left)], self.vlmcs[int(right)], weight=dist)
 
   cdef np.ndarray[FLOATTYPE_t, ndim=2] _calculate_distances(self):
     cdef int num_vlmcs = len(self.vlmcs)
@@ -53,6 +70,8 @@ cdef class GraphBasedClustering:
           distances[distances_index, 1] = right_i_t
           distances[distances_index, 2] = dist
           distances_index += 1
+
+          self.indexed_distances[left_i, right_i] = dist
 
     np.save(self.file_name, distances)
 
