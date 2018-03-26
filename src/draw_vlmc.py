d98405ad91e6cb6d8870ec8f1b7282e1d18090a1
Kalior <kalioragus@gmail.com>
2018-03-26 11:08:21 +0200

    Add illustration of shared contexts, fixes #42

--- src/draw_vlmc.py
+++ src/draw_vlmc.py
@@ -3,11 +3,23 @@
 import networkx as nx
 from networkx.drawing.nx_agraph import graphviz_layout
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import os
+from enum import Enum
+from itertools import combinations
 
 from vlmc import VLMC
 from parse_trees_to_json import parse_trees
 from get_signature_metadata import get_metadata_for
+
+mpl.rcParams['font.size'] = 24
+
+
+class Part(Enum):
+  LEFT = 1
+  INTERSECTION = 2
+  RIGHT = 3
+  NONE = 4
 
 
 def save(tree_dir, out_dir):
@@ -22,14 +34,14 @@ def save(tree_dir, out_dir):
 
   for vlmc in vlmcs:
     plt.figure(figsize=(150, 30), dpi=80)
-    vlmc.draw(metadata)
+    draw_with_probabilities(vlmc, metadata)
     out_file = os.path.join(out_dir, vlmc.name + '.pdf')
     plt.axis('off')
     plt.savefig(out_file, dpi='figure', format='pdf', bbox_inches='tight')
     plt.close()
 
 
-def draw(vlmc, metadata):
+def draw_with_probabilities(vlmc, metadata):
   G = nx.DiGraph()
   if vlmc.name in metadata:
     root_name = metadata[vlmc.name]['species']
@@ -63,7 +75,7 @@ def draw(vlmc, metadata):
   nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=12)
 
 
-def _add_children(vlmc, G, context, root_name):
+def add_children(vlmc, G, context, root_name):
   for c in vlmc.alphabet:
     child = c + context
     parent_label = context
@@ -73,14 +85,87 @@ def _add_children(vlmc, G, context, root_name):
       G.add_node(child, inner=True)
       G.add_edge(parent_label, child, symbol=c,
                  prob="{:.2f}".format(vlmc.tree[context][c]), inner=True)
-      vlmc._add_children(G, child, root_name)
+      add_children(vlmc, G, child, root_name)
     else:
       G.add_node(child, inner=False)
       G.add_edge(parent_label, child, symbol=c, prob="{:.2f}".format(
           vlmc.tree[context][c]), inner=False)
 
 
+def save_intersection(tree_dir, out_dir):
+  parse_trees(tree_dir)
+  vlmcs = VLMC.from_json_dir(tree_dir)
+  metadata = get_metadata_for([vlmc.name for vlmc in vlmcs])
+
+  try:
+    os.stat(out_dir)
+  except:
+    os.mkdir(out_dir)
+
+  for vlmc_left, vlmc_right in combinations(vlmcs, 2):
+    plt.figure(figsize=(90, 30), dpi=20)
+    draw_intersection(vlmc_left, vlmc_right, metadata)
+    out_file = os.path.join(out_dir, vlmc_left.name + '_' + vlmc_right.name + '.pdf')
+    plt.title(metadata[vlmc_left.name]['species'] +
+              " and " + metadata[vlmc_right.name]['species'])
+    plt.axis('off')
+    # plt.show()
+    plt.savefig(out_file, dpi='figure', format='pdf', bbox_inches='tight')
+    plt.close()
+
+
+def draw_intersection(vlmc_left, vlmc_right, metadata):
+  G = nx.DiGraph()
+  root_name = " "
+  G.add_node(root_name, part=Part.INTERSECTION)
+  add_children_intersection(vlmc_left, vlmc_right, G, "", root_name)
+  pos = graphviz_layout(G, prog='dot')
+
+  nodes = G.nodes(data='part')
+  intersection_nodes = [n[0] for n in nodes if n[1] == Part.INTERSECTION]
+  left_nodes = [n[0] for n in nodes if n[1] == Part.LEFT]
+  right_nodes = [n[0] for n in nodes if n[1] == Part.RIGHT]
+
+  edges = G.edges(data='part')
+  intersection_edges = [e for e in edges if e[2] == Part.INTERSECTION]
+  left_edges = [e for e in edges if e[2] == Part.LEFT]
+  right_edges = [e for e in edges if e[2] == Part.RIGHT]
+
+  nx.draw_networkx_nodes(G, pos, nodelist=intersection_nodes, node_size=100, node_color='#ff7f00')
+  nx.draw_networkx_nodes(G, pos, nodelist=left_nodes, node_size=100, node_color='#007fff')
+  nx.draw_networkx_nodes(G, pos, nodelist=right_nodes, node_size=100, node_color='#ff007f')
+  # nx.draw_networkx_labels(G, pos, font_size=16)
+
+  nx.draw_networkx_edges(G, pos, edgelist=intersection_edges, arrows=True,
+                         edge_color='#ff7f00', width=6, style='solid')
+  nx.draw_networkx_edges(G, pos, edgelist=left_edges, arrows=False,
+                         edge_color='#007fff', width=2, style='dashed')
+  nx.draw_networkx_edges(G, pos, edgelist=right_edges, arrows=False,
+                         edge_color='#ff007f', width=2, style='dashed')
+
+
+def add_children_intersection(vlmc_left, vlmc_right, G, context, root_name):
+  for c in vlmc_left.alphabet:
+    child = c + context
+    parent_label = context
+    if parent_label == "":
+      parent_label = root_name
+    if child in vlmc_left.tree and child in vlmc_right.tree:
+      part = Part.INTERSECTION
+    elif child in vlmc_left.tree:
+      part = Part.LEFT
+    elif child in vlmc_right.tree:
+      part = Part.RIGHT
+    else:
+      part = Part.NONE
+
+    if part != Part.NONE:
+      G.add_node(child, part=part)
+      G.add_edge(parent_label, child, symbol=c, part=part)
+      add_children_intersection(vlmc_left, vlmc_right, G, child, root_name)
+
+
 if __name__ == '__main__':
   tree_dir = '../trees_pst_better'
-  image_dir = '../images'
-  save(tree_dir, image_dir)
+  image_dir = '../vlmc_illustrations'
+  save_intersection(tree_dir, image_dir)
