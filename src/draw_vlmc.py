a8d61aa2ed9de8b2286daf1021ac14047595768e
Kalior <kalioragus@gmail.com>
2018-03-26 16:14:50 +0200

    Add option to draw deltas in the trees

--- src/draw_vlmc.py
+++ src/draw_vlmc.py
@@ -34,60 +34,68 @@ def save(tree_dir, out_dir, deltas=False):
 
   for vlmc in vlmcs:
     plt.figure(figsize=(150, 30), dpi=80)
-    draw_with_probabilities(vlmc, metadata)
+    draw_with_probabilities(vlmc, metadata, deltas)
     out_file = os.path.join(out_dir, vlmc.name + '.pdf')
     plt.axis('off')
     plt.savefig(out_file, dpi='figure', format='pdf', bbox_inches='tight')
     plt.close()
 
 
-def draw_with_probabilities(vlmc, metadata):
+def draw_with_probabilities(vlmc, metadata, deltas):
   G = nx.DiGraph()
   if vlmc.name in metadata:
     root_name = metadata[vlmc.name]['species']
   else:
     root_name = vlmc.name
-  G.add_node(root_name, inner=True)
-  add_children(vlmc, G, "", root_name)
+  G.add_node(root_name, inner=True, delta=-1)
+  add_children(vlmc, G, "", root_name, deltas)
+
+  pos = graphviz_layout(G, prog='dot')
+  nx.draw_networkx_nodes(G, pos, node_size=10, node_color='w')
+
+  if deltas:
+    nodes = G.nodes(data=True)
+    inner_nodes = {n[0]: n[1]['delta'] for n in nodes if n[1]['inner']}
+  else:
+    nodes = G.nodes(data='inner')
+    inner_nodes = {n[0]: n[0] for n in nodes if n[1]}
+
+  nx.draw_networkx_labels(G, pos, font_size=16, labels=inner_nodes)
 
   edges = G.edges(data='inner')
   inner_edges = [e for e in edges if e[2]]
   outer_edges = [e for e in edges if not e[2]]
-
-  nodes = G.nodes(data='inner')
-  inner_nodes = {n[0]: n[0] for n in nodes if n[1]}
-
-  pos = graphviz_layout(G, prog='dot')
-
-  nx.draw_networkx_nodes(G, pos, node_size=10, node_color='w')
-  nx.draw_networkx_labels(G, pos, font_size=16, labels=inner_nodes)
-
   nx.draw_networkx_edges(G, pos, edgelist=inner_edges, arrows=True, edge_color='#ff7f00',
                          width=2, style='solid')
 
-  nx.draw_networkx_edges(G, pos, edgelist=outer_edges, arrows=False, edge_color='#007fff',
-                         width=1, style='dashed')
+  if not deltas:
+    nx.draw_networkx_edges(G, pos, edgelist=outer_edges, arrows=False, edge_color='#007fff',
+                           width=1, style='dashed')
 
-  edge_attributes = ['symbol', 'prob']
+  if deltas:
+    edge_attributes = ['symbol']
+  else:
+    edge_attributes = ['symbol', 'prob']
+
   attributes = [nx.get_edge_attributes(G, attr) for attr in edge_attributes]
   keys = set([k for attrs in attributes for k in attrs.keys()])
   labels = {k: " ".join([attrs[k] for attrs in attributes if k in attrs]) for k in keys}
   nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=12)
 
 
-def add_children(vlmc, G, context, root_name):
+def add_children(vlmc, G, context, root_name, deltas):
   for c in vlmc.alphabet:
     child = c + context
     parent_label = context
     if parent_label == "":
       parent_label = root_name
     if child in vlmc.tree:
-      G.add_node(child, inner=True)
+      G.add_node(child, inner=True, delta="{:.2f}".format(vlmc.tree[context][c]))
       G.add_edge(parent_label, child, symbol=c,
                  prob="{:.2f}".format(vlmc.tree[context][c]), inner=True)
-      add_children(vlmc, G, child, root_name)
-    else:
-      G.add_node(child, inner=False)
+      add_children(vlmc, G, child, root_name, deltas)
+    elif not deltas:
+      G.add_node(child, inner=False, delta=-1.0)
       G.add_edge(parent_label, child, symbol=c, prob="{:.2f}".format(
           vlmc.tree[context][c]), inner=False)
 
@@ -166,6 +174,9 @@ def add_children_intersection(vlmc_left, vlmc_right, G, context, root_name):
 
 
 if __name__ == '__main__':
+  # tree_dir = '../original_test_trees'
+  # image_dir = '../vlmc_illustrations'
+  # save_intersection(tree_dir, image_dir)
   tree_dir = '../trees_pst_better'
-  image_dir = '../vlmc_illustrations'
-  save_intersection(tree_dir, image_dir)
+  image_dir = '../images'
+  save(tree_dir, image_dir, False)
