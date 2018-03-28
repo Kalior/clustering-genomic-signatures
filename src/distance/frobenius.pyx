380d5acde7e2574bf0981695a6c6256f9119f177
Erik Norlander <e.olof.norlander@gmail.com>
2018-03-28 10:18:15 +0200

    Use the root mean square of the union of all contexts

--- src/distance/frobenius.pyx
+++ src/distance/frobenius.pyx
@@ -21,17 +21,17 @@ cdef class FrobeniusNorm(object):
 
   cdef double _frobenius_norm(self, left_vlmc, right_vlmc):
     # intersection:
-    cdef list shared_contexts = [context for context in left_vlmc.tree if context in right_vlmc.tree]
+    # cdef list shared_contexts = [context for context in left_vlmc.tree if context in right_vlmc.tree]
 
     # union
-    # cdef set shared_contexts = set(left_vlmc.tree.keys()).union(set(right_vlmc.tree.keys()))
+    cdef set shared_contexts = set(left_vlmc.tree.keys()).union(set(right_vlmc.tree.keys()))
 
     cdef np.ndarray[FLOATTYPE_t, ndim=2] left_matrix = self._create_matrix(left_vlmc, shared_contexts)
     cdef np.ndarray[FLOATTYPE_t, ndim=2] right_matrix = self._create_matrix(right_vlmc, shared_contexts)
 
     cdef np.ndarray[FLOATTYPE_t, ndim=2] frobenius_matrix = left_matrix - right_matrix
 
-    return np.linalg.norm(frobenius_matrix, ord='fro') / len(shared_contexts)
+    return np.linalg.norm(frobenius_matrix, ord='fro') / np.sqrt(len(shared_contexts))
 
   cdef np.ndarray[FLOATTYPE_t, ndim=2] _create_matrix(self, vlmc, contexts_to_use):
     cdef int rows = len(contexts_to_use)
