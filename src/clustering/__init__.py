ab45956749701d864b2ceed882df67ede79cb82c
Erik Norlander <e.olof.norlander@gmail.com>
2018-04-13 08:56:41 +0200

    Add class that contains a clustering and can be used to get metrics

--- src/clustering/__init__.py
+++ src/clustering/__init__.py
@@ -3,3 +3,5 @@ from .mst_clustering import MSTClustering
 from .average_link_clustering import AverageLinkClustering
 from .k_means import KMeans
 from .fuzzy_similarity_clustering import FuzzySimilarityClustering
+from .clustering_metrics import ClusteringMetrics
+
