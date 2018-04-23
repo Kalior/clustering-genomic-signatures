dc8d87653facc5091e5272f807451eaf86298823
Kalior <kalioragus@gmail.com>
2018-04-23 14:24:23 +0200

    Fetch baltimore and sequence length from db

--- src/get_signature_metadata.py
+++ src/get_signature_metadata.py
@@ -5,23 +5,45 @@ import json
 def get_metadata_for(signatures):
   config = json.load(open('db_config.json'))
   cnx = mysql.connector.connect(**config)
+
+  sequence_lengths = get_sequence_lengths(signatures, cnx)
   cursor = cnx.cursor()
 
   signature_aids = ', '.join(
       ["'{}'".format(signature) for signature in signatures])
 
-  query = ("select aid, ord, fam, sub, gen, spc, organism from virus where aid in ({})".format(signature_aids))
+  query = ("select aid, ord, fam, sub, gen, spc, blt, organism from virus where aid in ({})".format(
+      signature_aids))
 
   cursor.execute(query)
 
   metadata = {}
-  for (aid, ord_, fam, sub, gen, spc, organism) in cursor:
+  for (aid, ord_, fam, sub, gen, spc, blt, organism) in cursor:
     metadata[aid] = {'order': ord_, 'family': fam, 'subfamily': sub,
-                     'genus': gen, 'species': spc, 'organism': organism}
+                     'genus': gen, 'species': spc, 'baltimore': blt, 'organism': organism,
+                     'sequence_length': sequence_lengths[aid]}
 
   cnx.close()
 
   return metadata
+
+
+def get_sequence_lengths(signatures, cnx):
+  cursor = cnx.cursor()
+
+  signature_aids = ', '.join(
+      ["'{}'".format(signature) for signature in signatures])
+
+  query = ("select aid, seq from genome where aid in ({})".format(
+      signature_aids))
+
+  cursor.execute(query)
+
+  sequence_lengths = {}
+  for (aid, seq) in cursor:
+    sequence_lengths[aid] = len(seq)
+
+  return sequence_lengths
 
 if __name__ == "__main__":
   metadata = get_metadata_for(['AB026117.1', 'AC_000005.1'])
